e508e0912581553b5004f21250fb3e3fd42bd0fb
Mohammad Taghizadeh <taghizadeh.mhmd@gmail.com>
2020-03-24 01:04:19 +0430

    separate views from app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from Config import Development
 
 app = Flask(__name__)
+app.config.from_object(Development)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
+# import blueprints
 from mod_admin import admin
+from mod_users import users
+
 app.register_blueprint(admin)
+app.register_blueprint(users)
