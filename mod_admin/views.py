4908c10680cc7af9018e9c43e2e18f6ca0476f20
Mohammad Taghizadeh <taghizadeh.mhmd@gmail.com>
2020-03-25 23:21:13 +0430

    20 - flash messages

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,5 +1,5 @@
 from . import admin ### . means __init__.py
-from flask import session, render_template, request, abort ### abort for errors
+from flask import session, render_template, request, abort, flash ### abort for errors
 from mod_users.froms import LoginForm
 from mod_users.models import User
 
@@ -25,10 +25,16 @@ def login():
 
         user = User.query.filter(User.email.ilike(f'{form.email.data}')).first() # Incase Sensetive Like () ---> ilike() in sql alchemy
         if not user:
-            return "Incorrect Credentials!!!", 400 ### status code 400 rather than 200
-        
+            # return "Incorrect Credentials!!!", 400 ### status code 400 rather than 200
+            
+            ### (flash message save in session)     
+            flash("Incorrect Credentials!!!", category="error") ### you must set flash message in view 
+            print(session)
+            return render_template("admin/login.html", form=form)
         if not user.check_pass(form.password.data):
-            return "Incorrect Password"
+            flash("Incorrect Password", category="error")
+            print(session)
+            return render_template("admin/login.html", form=form)
 
         session['email'] = user.email
         session['user_id'] = user.id
