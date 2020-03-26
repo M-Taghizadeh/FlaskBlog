1b58a9726d1b07a863528a86a293205529ab7308
Mohammad Taghizadeh <taghizadeh.mhmd@gmail.com>
2020-03-26 22:48:21 +0430

    23 - Handling Database Errors

--- mod_users/views.py
+++ mod_users/views.py
@@ -5,6 +5,7 @@ from .froms import RegisterForm
 from flask import request, render_template, flash
 from .models import User
 from app import db
+from sqlalchemy.exc import IntegrityError
 
 @users.route("/register/", methods=["GET", "POST"])
 def register():
@@ -19,6 +20,12 @@ def register():
             form.confirm_password.errors.append(error_msg)
             return render_template("users/register.html", form = form)
 
+        ### Method 1 for handeling user duplicated:
+        old_user = User.query.filter(User.email.ilike(form.email.data)).first()
+        if old_user:
+            flash("Email in use.", category="error")
+            return render_template("users/register.html", form = form)
+
         new_user = User()
         new_user.full_name = form.full_name.data
         new_user.email = form.email.data
@@ -26,5 +33,15 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         flash("Your Welcome dear " + form.full_name.data, category="tertiary")
+        
+        ### Method 2 for handeling user duplicated:
+        # try:
+        #     db.session.add(new_user)
+        #     db.session.commit()
+        #     flash("Your Welcome dear " + form.full_name.data, category="tertiary")
+        # except IntegrityError:
+        #     db.session.rollback()
+        #     flash("Email in use.", category="error")
+
 
     return render_template("users/register.html", form = form)
