5fec9c20017d93fb66dc783ab7eb979f10ae95d0
Mohammad Taghizadeh <taghizadeh.mhmd@gmail.com>
2020-03-27 20:01:40 +0430

    25 - Relations in SQLAlchemy

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -1,5 +1,11 @@
 from app import db
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, Table, ForeignKey
+
+
+Posts_Categories = Table('Posts_Categories', db.metadata,
+    Column('post_id', Integer, ForeignKey('Posts.id', ondelete='cascade')),
+    Column('category_id',  Integer, ForeignKey('Categories.id', ondelete='cascade'))
+)
 
 class Category(db.Model):
     __tablename__ = "Categories"
@@ -8,6 +14,10 @@ class Category(db.Model):
     description = Column(String(256), nullable=True) 
     slug = Column(String(128), nullable=False, unique=True) 
 
+    # define a relationship (dont need to flask db migrate , that use in python side)
+    # relationship ('related_class_name', secondary=middle_tbl_name, back_populates='this_tbl_name')
+    posts = db.relationship('Post', secondary=Posts_Categories, back_populates='categories')
+
 class Post(db.Model):
     __tablename__ = "Posts"
     id = Column(Integer, primary_key=True) 
@@ -15,3 +25,7 @@ class Post(db.Model):
     summary = Column(String(256), nullable=True) 
     content = Column(Text, nullable=False) 
     slug = Column(String(128), nullable=False, unique=True) 
+
+    # define a relationship (dont need to flask db migrate , that use in python side)
+    # relationship ('related_class_name', secondary=middle_tbl_name, back_populates='this_tbl_name')
+    categories = db.relationship('Category', secondary=Posts_Categories, back_populates='posts')
